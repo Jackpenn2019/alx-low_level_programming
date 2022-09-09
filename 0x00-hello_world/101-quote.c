6e8673d7142d8cf59d55dae32f46016d1237e60d
jackpenn2019 <kibet.issa@gmail.com>
2022-09-09 08:08:42 -0700

    correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> #include <stdlib.h>
 
 /**
- *  main - prints out and that piece of
- *  art is useful" - Dora Korpar, 2015-10-19
- *  Return: returns 1
- */
-int main(void)
-{
-	 fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	 exit (1);
+ *  main - prints out and that piece of *  art is useful" - Dora Korpar,
+ 2015-10-19 *  Return: returns 1 */
+int main(void) {
+	 fprintf(stderr,"and that piece of art is useful\" - Dora Korpar,
+	 2015-10-19\n"); exit (1);
 }
