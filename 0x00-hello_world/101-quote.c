62a29b6d8fe04de48a50873f8c6a4dfbc537d000
jackpenn2019 <kibet.issa@gmail.com>
2022-09-09 08:21:26 -0700

    latest

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,12 @@
 #include <stdlib.h>
 
 /**
- *  main - prints out and that piece of 
- *  art is useful" - Dora Korpar, 2015-10-19
- *  Return: returns 1
- */ 
+ *  main - prints and that piece of art
+ *  is useful" - Dora Korpar, 2015-10-19
+ *  Return: return 1
+ */
 int main(void)
 {
-	 fprintf(stderr,"and that piece of art is useful\" - Dora Korpar,
-	 2015-10-19\n");
-	 exit (1);
+	fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	exit (1);
 }
