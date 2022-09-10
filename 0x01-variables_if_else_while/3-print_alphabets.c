ebc3ee62457f429767a5423be8f72c642343efc3
jackpenn2019 <kibet.issa@gmail.com>
2022-09-09 17:46:26 -0700

    capital

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *  main - rints the alphabet in lowercase, followed by a new line
+ *  Return: return 0
+ */
+
+int main(void)
+{
+char i;
+
+for (i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+for (i = 'A'; i <= 'Z'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
