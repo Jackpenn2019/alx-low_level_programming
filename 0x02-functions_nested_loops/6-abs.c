76690f95fc4c2147bf71120fe1fe0f9ed3af58b5
jackpenn2019 <kibet.issa@gmail.com>
2022-09-13 16:43:19 -0700

    first time

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @int: pamater passed
+ *
+ * Return: absolute value
+ */
+
+int _abs(int c)
+{
+	return (abs(c));
+}
