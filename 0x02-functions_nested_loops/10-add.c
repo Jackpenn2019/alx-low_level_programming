85d6f9250e805b395545195adbf16c96d88b2591
jackpenn2019 <kibet.issa@gmail.com>
2022-09-13 17:48:21 -0700

    sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ * @n, @m: integers passed
+ *
+ * Return: always integer
+ */
+
+int add(int n, int m)
+{
+	return (n + m);
+
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,5 +11,6 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void print_to_98(int n);
+int add(int, int);
 
 #endif
