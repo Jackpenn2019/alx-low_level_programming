37be146ec57fac63cd9602f3addf8fe101682d42
jackpenn2019 <kibet.issa@gmail.com>
2022-09-13 17:41:52 -0700

    tough

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - prints to 98
+ * @n: parameter passed
+ *
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+
+			if (n != 98)
+			{
+				printf(", ");
+			}
+
+			n--;
+		}
+	}
+	else if (n < 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+
+			if (n != 98)
+			{
+				printf(", ");
+			}
+
+			n++;
+		}
+	}
+	else 
+	{
+		printf("98");
+	}
+	printf("\n");
+
+}
+
