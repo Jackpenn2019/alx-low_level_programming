09e8d7d956da8a2a5709a6330e50f88f56622caf
jackpenn2019 <kibet.issa@gmail.com>
2022-09-15 13:13:15 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *  print_numbers - print numbers
+ *  from 0 to 9
+ *
+ *  Return: void
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
