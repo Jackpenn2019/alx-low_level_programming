369743b776987d75db7217c3416d7606bc761a96
jackpenn2019 <kibet.issa@gmail.com>
2022-09-15 14:35:16 -0700

    correction

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- *  print_line - prints a straight line
- *  @n: number of times to print
+ *  print_line - prints a line
+ *  @n: number of times
  *
  *  Return: void
  */
@@ -11,7 +11,7 @@ void print_line(int n)
 {
 	int x;
 
-	for (x = 1; x <= n; x++)	
+	for (x = 1; x <= n; x++)
 	{
 		if (n <= 0)
 		{
