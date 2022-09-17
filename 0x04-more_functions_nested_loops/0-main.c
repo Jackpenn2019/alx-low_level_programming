19e77fa7df14bd09a4f3a8b6dbbc9573a2fa0e8f
jackpenn2019 <kibet.issa@gmail.com>
2022-09-16 20:10:40 -0700

    fixes

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * main - test some files
+ * main - test files
  * Return: void
  */
-
 int main(void)
 {
-int i;
+	int i;
 
 	i = 0;
+
 	positive_or_negative(i);
 
-return (0);
+	return (0);
 }
