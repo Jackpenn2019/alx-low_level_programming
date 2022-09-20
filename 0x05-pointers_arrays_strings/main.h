4d7179fb43ecc3396cb7fed3c2d768ed72f89cb6
jackpenn2019 <kibet.issa@gmail.com>
2022-09-19 19:03:23 -0700

    final

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -12,5 +12,7 @@ void puts2(char *str);
 int _putchar(char c);
 void puts_half(char *str);
 void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+void print_rev(char *s);
 
 #endif
