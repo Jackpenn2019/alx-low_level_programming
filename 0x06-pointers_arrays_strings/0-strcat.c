71ac9b66c0bad9f0e1dc34d3cfb549175d17b9b2
jackpenn2019 <kibet.issa@gmail.com>
2022-09-22 16:05:21 -0700

    first commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strcat: concatenates two strings
+ * @dest: input parameter 1
+ * @src: input parameter 2
+ *
+ * Return: pointer to a variable
+ * of char type
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	dest = strcat(dest, src);
+
+	return dest;
+}
