367212cdd0173ac32baf6cda18bc7dd3af2c3ecb
jackpenn2019 <kibet.issa@gmail.com>
2022-09-22 17:16:00 -0700

    first commit

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: always integer
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	if (s1 == s2)
+	{
+		result = 0;
+	}
+	else if (s2 > s1)
+	{
+		result = -1;
+	}
+	else 
+	{
+		result = 1;
+	}
+
+	return (result);
+}
