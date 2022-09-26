e39db088ac6f41de69e95f0fb883622d5385c6f0
jackpenn2019 <kibet.issa@gmail.com>
2022-09-26 15:03:01 -0700

    first commit

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies n bytes from memory
+ * area src to memory area dest
+ * @dest: to memory area
+ * @src: from memory area
+ * n: number of bytes to copy
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i = 0;
+
+	while (n--)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
