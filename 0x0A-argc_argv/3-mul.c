b398142e194fb5551d01e7d90874e9741df27632
jackpenn2019 <kibet.issa@gmail.com>
2022-10-04 15:28:27 -0700

    multiply

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - main method
+ * @argc: first argument
+ * @argv: second argument
+ *
+ * Return: integer
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc < 3)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	else
+	{
+		int result;
+		result = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", result);
+	}
+	return (0);
+}
