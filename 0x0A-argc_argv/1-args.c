e6c33bf90d3a2fad027f072baccda63c97f9c1f0
jackpenn2019 <kibet.issa@gmail.com>
2022-10-04 15:10:05 -0700

    first commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - main method
+ * @argc: first argument
+ * @argv: second argument
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
