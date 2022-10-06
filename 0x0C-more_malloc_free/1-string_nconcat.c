5634b6e6ba1a91a6314a7f45b00d598833325374
jackpenn2019 <kibet.issa@gmail.com>
2022-10-06 13:25:45 -0700

    fixed

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -30,7 +30,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; i < len1; i++)
 		result[i] = s1[i];
 
-	for(j = 0; j < len2; j++, i++)
+	for (j = 0; j < len2; j++, i++)
 		result[i] = s2[j];
 
 	result[i] = '\0';
