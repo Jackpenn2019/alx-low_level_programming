bad060b1f39bca038976fe588a4d6f994d0ec094
jackpenn2019 <kibet.issa@gmail.com>
2022-10-25 16:17:17 -0700

    some fix

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to the first node
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *ptr;
+
+	while (head)
+	{
+		ptr = head->next;
+		free(head->str);
+		free(head);
+		head = ptr;
+	}
+}
